368c05cc98ba4e2e6473e3b50a2af8ebaa881c07
中原賢一 <ohagurokenichi@yahoo.co.jp>
2018-07-16 22:03:33 +0900

    Added radio_button function

--- start.js
+++ start.js
@@ -1,19 +1,29 @@
 localStorage["start"] = 1;
 console.log("スタート");
 var n = 1;
+console.log(radio)
+console.log(typeof radio)
+console.log(foots)
+console.log(typeof foots)
 
-var timer1 = setInterval("reload()",foots * 10000);
+if (radio == "airbnb") {
+  var timer1 = setInterval("reload()",foots * 10000);
 
-function reload() {
-  if ( localStorage["start"] == 1 ) {
-  	c = document.getElementsByClassName('_1rp5252');
-
-    for (var i = 0; i<c.length; i=i+2) {
-      c[i].click();
-      console.log(n+"リロード");
-      n++;
-      };
-   } else {
-    clearInterval(timer1);
-   }
-}
+  function reload() {
+    if ( localStorage["start"] == 1 ) {
+      c = document.getElementsByClassName('_1rp5252');
+  
+      for (var i = 0; i<c.length; i=i+2) {
+        c[i].click();
+        console.log(n+"リロード");
+        n++;
+        };
+    } else {
+      clearInterval(timer1);
+    }
+  }
+} else {
+  alert("正しいアプリケーションを設定して下さい")
+  localStorage["start"] = 0;
+  console.log("停止");
+};
